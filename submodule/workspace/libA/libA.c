a18b9ab4a57d4e866271c2a113f8f0fd916f1321
Frankzd <576613133@qq.com>
2018-12-05 00:42:00 +0800

    gitlearn_submodule

---
+++ submodule/workspace/libA/libA.c
@@ -0,0 +1,19 @@
+/*
+ * @Author: mikey.zhaopeng 
+ * @Date: 2018-12-05 00:32:19 
+ * @Last Modified by:   mikey.zhaopeng 
+ * @Last Modified time: 2018-12-05 00:32:19 
+ */
+
+#include <stdio.h>
+
+/**
+ * @brief 
+ * 
+ * @return int 
+ */
+int main()
+{
+    printf("this is libA.git!\n");
+    return 0;
+}
